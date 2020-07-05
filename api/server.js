dc5ab5d08c7267601ab326b71cfdd3b6b07be4ed
schang1146 <sammychang2185@gmail.com>
2020-07-05 14:56:46 -0700

    added routes section and initialized projectRouter for later use

--- api/server.js
+++ api/server.js
@@ -4,6 +4,9 @@ const express = require('express');
 /* server pipeline */
 const server = express();
 
+/* import routes */
+// server.use('/api/projects', projectRouter);
+
 /* GET '/' */
 server.get('/', (req, res) => {
     res.send('Hello from Express!');
